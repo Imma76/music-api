d427965b3514237bb1d6210e45c8db9f0e64c46e
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-23 17:26:46 +0100

    feat:error handler'

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,4 +1,4 @@
-const errorHander = (error, req, res, next) => { return res.status(500).send({ status: false, message: `${error}` }); }
+const errorHandler = (error, req, res, next) => { return res.status(500).send({ status: false, message: `${error}` }); }
 
 
 export default errorHandler;
