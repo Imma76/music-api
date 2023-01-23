40c1b77c7b0f5b38d9de1133ad2ced1431c79fab
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-23 15:56:39 +0100

    user model

---
+++ model/user.model.js
@@ -0,0 +1,16 @@
+import sequelize from '../config/db.config.js';
+import { Sequelize } from 'sequelize';
+
+const User = sequelize.define('User',{
+    id:{
+        autoIncrement:true,
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+    },
+    name: {
+        type:Sequelize.STRING,
+    },
+    email: {
+        type:Sequelize.STRING,
+    }
+})
