ab130df35a854559c8d24e0889affd52ad8a133f
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-23 18:49:15 +0100

    init album routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import userRouter from './user.routes.js';
+import albumRouter from './album.routes.js';
 
 const indexRouter = express.Router();
 
 
 indexRouter.use('/user',userRouter);
+indexRouter.use('/album', albumRouter);
 export default indexRouter;
