9c0e639432255a1708937484ad96597722daa502
Emmanuel <emmanuelugwueze6@gmail.com>
2023-01-23 16:17:43 +0100

    init index router

--- middlewares/index.middleware.js
+++ middlewares/index.middleware.js
@@ -1,11 +1,17 @@
 import sequelize from '../config/db.config.js';
+import indexRouter from '../routes/index.routes.js';
+import express from 'express';
 
 const middleware = (app) => {
+    app.use(express.json());
+    app.use(express.urlencoded
+        ({ extended: false }));
     sequelize.sync().then(val => {
         console.log('db connected')
     }).catch(err=>{
         console.log(`${err}`)
     })
+    app.use(indexRouter);
 }
 
 
