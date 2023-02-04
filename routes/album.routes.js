bab528b42fe6ab126642e2bad092e9f3791541fa
Emmanuel <emmanuelugwueze6@gmail.com>
2023-02-04 01:16:53 +0100

    delete album route

--- routes/album.routes.js
+++ routes/album.routes.js
@@ -6,5 +6,6 @@ const albumRouter = express.Router();
 
 albumRouter.post('/',albumController.createAlbum);
 albumRouter.put('/update',albumController.updateAlbumSongs);
+albumRouter.delete('/:albumId', albumController.deleteAlbum);
 
 export default albumRouter;
