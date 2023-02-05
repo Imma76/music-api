20b0919b56a228c17e342b5900987facc7aba02a
Emmanuel <emmanuelugwueze6@gmail.com>
2023-02-05 18:26:46 +0100

    create song funcction

--- controllers/song.controller.js
+++ controllers/song.controller.js
@@ -5,8 +5,16 @@ import Song from "../model/song.model.js";
 class SongController{
     async createSong(req, res) {
         const create = Song.create
-            ({ artistId: req.body.artistId, songLink:req.body.songLink });
+            ({ artistId: req.body.artistId, songLink:req.body.songLink }).then(val=>{
+
+                return res.status(201).send({status:true,message:'song created successfully'})
+            }).catch(err =>{
+                return res.status(500).send({ status: false, message: `${err}` });
+            });
     }
+
+
+    
 }
 
 
