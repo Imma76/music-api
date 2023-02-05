9613416374d07656bcc5131d9609a441e57876e8
Emmanuel <emmanuelugwueze6@gmail.com>
2023-02-05 18:36:47 +0100

    feat:update artist song

--- controllers/song.controller.js
+++ controllers/song.controller.js
@@ -13,6 +13,22 @@ class SongController{
             });
     }
 
+    async updateSong(req,res){
+
+        const update = Song.findOne
+            ({ where: { id: req.body.songId } }).then(val => {
+
+                val.songLink = req.body.songLink;
+                Song.update({ songLink: val.songLink, }, { id: req.body.songId }).then(val => {
+                     res.status(200).send({status:true,message:'song updated successfully'})
+                }).catch(err => {
+                     res.status(500).send({status:false,message:`${err}`})
+                 })
+        }).catch(err => {
+            res.status(500).send({status:false,message:`${err}`})
+        })
+    }
+
 
     
 }
